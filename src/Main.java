0139865f5800c74342a53eb054538838e9666edd
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-03-28 20:30:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 1200;
+        int mile = ticketPrice/20;
+        System.out.println("Вам начислено " + mile+ " бонусов");
+    }
+}
